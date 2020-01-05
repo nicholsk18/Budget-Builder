5ce2bb1dd209ce0a7f0f1db0ed88d971039c97ff
Karson Nichols <nicholsk18@gmail.com>
2020-01-05 12:59:30 -0600

    added practice gui

--- src/learn.py
+++ src/learn.py
@@ -1,24 +1,59 @@
-from tkinter import *
+#!/usr/bin/env python
+import wx
+import os
 
 
-class Window(Frame):
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.master = master
+class MainWindow(wx.Frame):
+    """We simply derive a new class of a frame"""
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title, size=(500, 500))
+        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.CreateStatusBar() # A status bar in the bottom of the window
 
-        self.pack(fill=BOTH, expand=1)
-        print(self)
-        exitButton = Button(self, text="Button", fg="#ff00ff", command=self.clickExitButton)
+        # setting up the menu
+        filemenu = wx.Menu()
 
-        exitButton.place(x=200, y=200)
+        # wx.ID_ABOUT and wx.ID_EXIT are standard ids provided by wxWidgets.
+        menuOpen = filemenu.Append(wx.ID_OPEN, "&Open", " Open file")
+        menuAbout = filemenu.Append(wx.ID_ABOUT, "&About", " Information about this program")
+        menuExit = filemenu.Append(wx.ID_EXIT, "E&xit", " Terminate the program")
 
-    def clickExitButton(self):
-        exit()
+        # Creating the menu bar
+        menuBar = wx.MenuBar()
+        menuBar.Append(filemenu, "&File")
+        self.SetMenuBar(menuBar)
+
+        # Set events
+        self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
+        self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
+        self.Bind(wx.EVT_MENU, self.OnOpen, menuOpen)
+
+        self.Show(True)
+
+    # This is where opeing pdf file will go
+    def OnOpen(self, e):
+        """ Open a File """
+        self.dirname = ''
+        dlg = wx.FileDialog(self, "Choose a file", self.dirname, "", "*.*", wx.FD_OPEN)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.filename = dlg.GetFilename()
+            self.dirname = dlg.GetDirectory()
+            f = open(os.path.join(self.dirname, self.filename), 'r')
+            self.control.SetValue(f.read())
+            f.close()
+        dlg.Destroy()
+
+    def OnAbout(self, e):
+        # A message dialog box with an ok button. wx.OK is a standard ID in wxWidgets.
+        # wx.OK is an ID that is not needed
+        dlg = wx.MessageDialog(self, "A small text editor", "About Sample Editor", wx.OK)
+        dlg.ShowModal()  # Show it
+        dlg.Destroy()  # finally destroy it when finished
+
+    def OnExit(self, e):
+        self.Close(True)  # Close the frame.
 
 
-root = Tk()
-app = Window(root)
-
-root.wm_title("Tkinter window")
-root.geometry("750x750")
-root.mainloop()
+app = wx.App(False)
+frame = MainWindow(None, 'Small editor')
+app.MainLoop()
